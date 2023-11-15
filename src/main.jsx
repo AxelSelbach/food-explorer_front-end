2467a6d0d5ca45bc2c9e406d06c0553cc1928a34
axel <axelselbach90@gmail.com>
2023-11-15 18:18:41 -0300

    feat: encompassed the app in global style and theme context

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ThemeProvider } from 'styled-components'
 import { App } from './App.jsx'
+import GlobalStyle from './styles/global.js'
+import theme from './styles/theme.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle>
+        <App />
+      </GlobalStyle>
+    </ThemeProvider>
   </React.StrictMode>,
 )
