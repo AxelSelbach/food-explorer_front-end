bc7f6b17ee82fc0fdc39c54e9097af514126d0af
axel <axelselbach90@gmail.com>
2023-11-21 10:50:27 -0300

    feat: created header component style

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,22 @@
+import headerLogo from '../../assets/headerLogo.png'
+import nota from '../../assets/nota.png'
 import { Container } from './styles'
 
 export function Header() {
   return (
     <Container>
-      <h1></h1>
+      <nav className="center">
+        <div></div>
+      </nav>
+
+      <div className="centerLogo">
+        <img src={headerLogo} alt="Polígono" />
+        <h1>Food Explorer</h1>
+      </div>
+
+      <button>
+        <img src={nota} alt="" />
+      </button>
     </Container>
   )
 }
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -7,5 +7,50 @@ export const Container = styled.header`
   background-color: ${({ theme }) => theme.COLORS.BACKGROUND_900};
 
   display: flex;
+  flex-direction: row;
   justify-content: space-between;
+  align-items: center;
+  padding: 56px 28px 24px;
+
+  > .center {
+    width: 50px;
+    height: 18px;
+    border: none;
+  }
+
+  .center:before,
+  .center:after,
+  .center div {
+    background: ${({ theme }) => theme.COLORS.COLOR_WHITE};
+    content: '';
+    display: block;
+    height: 6px;
+    border-radius: 3px;
+    margin: 7px 0;
+    transition: 0.5s;
+  }
+  .center:hover:before {
+    transform: translateY(12px) rotate(135deg);
+  }
+  .center:hover:after {
+    transform: translateY(-12px) rotate(-135deg);
+  }
+  .center:hover div {
+    transform: scale(0);
+  }
+
+  > .centerLogo {
+    display: flex;
+    align-items: center;
+    gap: 8px;
+
+    > h1 {
+      color: ${({ theme }) => theme.COLORS.COLOR_WHITE};
+    }
+  }
+
+  > button {
+    background-color: transparent;
+    border: none;
+  }
 `
