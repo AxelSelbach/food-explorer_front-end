9ece13f16374f20ffcf42191b83d23535ad30291
axel <axelselbach90@gmail.com>
2023-11-21 09:55:07 -0300

    fix: adjusted footer style

--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -2,28 +2,29 @@ import styled from 'styled-components'
 
 export const Container = styled.footer`
   width: 100vw;
-  height: 20vh;
+  height: 10vh;
+  padding: 24px 20px;
   display: flex;
   flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
   gap: 8px;
   background-color: ${({ theme }) => theme.COLORS.BACKGROUND_600};
 
   > div {
     color: ${({ theme }) => theme.COLORS.DARK_SLATE_GRAY};
     font-family: Roboto;
-    font-size: 15px;
-    font-style: normal;
+    font-size: 12px;
     font-weight: 700;
-    line-height: normal;
+    display: flex;
+    flex-direction: row;
+    gap: 10px;
   }
 
   > h2 {
-    color: ${({ theme }) => theme.COLORS.WHITE};
-    text-align: right;
+    color: ${({ theme }) => theme.COLORS.COLOR_WHITE};
     font-family: DM Sans;
     font-size: 12px;
-    font-style: normal;
     font-weight: 400;
-    line-height: normal;
   }
 `
