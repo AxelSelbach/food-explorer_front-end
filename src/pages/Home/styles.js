319e1f86f67ae4b8a5562b0c2770753354bf17e4
axel <axelselbach90@gmail.com>
2023-11-28 09:49:44 -0300

    style: defined grid template rows

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -4,7 +4,7 @@ export const Container = styled.main`
   width: 100vw;
   height: 100vh;
   display: grid;
-  grid-template-rows: 10vh 80vh auto;
+  grid-template-rows: 10vh 80vh 10vh;
   grid-template-areas:
     'header'
     'content'
