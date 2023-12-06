c66ef092e583325f1be46a4ef8d89bcf089e03a2
axel <axelselbach90@gmail.com>
2023-12-06 17:05:15 -0300

    feat: added icon to text button

--- src/components/ButtonText/index.jsx
+++ src/components/ButtonText/index.jsx
@@ -1,5 +1,10 @@
 import { Container } from './styles'
 
-export function ButtonText({ title }) {
-  return <Container type="button">{title}</Container>
+export function ButtonText({ title, icon: Icon }) {
+  return (
+    <Container type="button">
+      {Icon && <Icon size={20} />}
+      {title}
+    </Container>
+  )
 }
