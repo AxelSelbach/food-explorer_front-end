f6f87d882e9a0b09c84e116788740e66f682db32
axel <axelselbach90@gmail.com>
2023-12-06 08:13:53 -0300

    feat: sign in page layout done

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,4 +1,5 @@
-import { FaCoffee } from 'react-icons/fa'
+import { FaEnvelope, FaKey } from 'react-icons/fa'
+import { FiCoffee } from 'react-icons/fi'
 import { ButtonText } from '../../components/ButtonText'
 import { Input } from '../../components/Input'
 import { Container, Form } from './styles'
@@ -7,11 +8,31 @@ export function SignIn() {
   return (
     <Container>
       <Form>
-        <h1>
-          <FaCoffee /> Food Explorer
-        </h1>
-        <Input />
-        <Input />
+        <div className="title">
+          <FiCoffee />
+          <h1>Food Explorer</h1>
+        </div>
+
+        <div className="input-wrapper">
+          <label htmlFor="email">Email</label>
+          <Input
+            type="text"
+            icon={FaEnvelope}
+            id="email"
+            placeholder="exemplo@exemplo.com.br"
+          />
+        </div>
+
+        <div className="input-wrapper">
+          <label htmlFor="password">Senha</label>
+          <Input
+            type="text"
+            icon={FaKey}
+            id="password"
+            placeholder="Senha de no mínimo 6 caracteres"
+          />
+        </div>
+
         <button>Entrar</button>
         <ButtonText title={'Criar uma conta'} />
       </Form>
--- src/pages/SignIn/styles.js
+++ src/pages/SignIn/styles.js
@@ -3,10 +3,15 @@ import styled from 'styled-components'
 export const Container = styled.div`
   height: 100dvh;
   width: 100dvw;
+  margin-top: 80px;
+
+  font-family: Roboto;
+
   background-color: ${({ theme }) => theme.COLORS.BACKGROUND_800};
+
   display: flex;
   flex-direction: column;
-  align-items: center;
+  align-items: stretch;
 `
 
 export const Form = styled.form`
@@ -15,6 +20,39 @@ export const Form = styled.form`
   align-items: center;
   justify-content: center;
   text-align: center;
+  gap: 32px;
 
-  padding: 80px 32px;
+  padding: 40px 48px;
+
+  > .title {
+    display: flex;
+    align-items: center;
+    gap: 16px;
+    margin-bottom: 66px;
+
+    color: ${({ theme }) => theme.COLORS.COLOR_WHITE};
+
+    h1 {
+      font-family: Roboto;
+      font-size: 32px;
+    }
+
+    > svg {
+      font-size: 32px;
+    }
+  }
+
+  > .input-wrapper {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 8px;
+
+    width: 100%;
+    color: ${({ theme }) => theme.COLORS.LIGHT_GRAY};
+  }
+
+  svg {
+    color: ${({ theme }) => theme.COLORS.CYAN_ORIENT};
+  }
 `
