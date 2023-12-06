adb58120685251f4c31edf7f158be69e58c70d97
axel <axelselbach90@gmail.com>
2023-12-06 10:17:36 -0300

    feat: finished sign up page

---
+++ src/pages/SignUp/index.jsx
@@ -0,0 +1,51 @@
+import { FaEnvelope, FaKey, FaRegUserCircle } from 'react-icons/fa'
+import { FiCoffee } from 'react-icons/fi'
+import { Button } from '../../components/Button'
+import { ButtonText } from '../../components/ButtonText'
+import { Input } from '../../components/Input'
+import { Container, Form } from './styles'
+
+export function SignUp() {
+  return (
+    <Container>
+      <Form>
+        <div className="title">
+          <FiCoffee />
+          <h1>Food Explorer</h1>
+        </div>
+
+        <div className="input-wrapper">
+          <label htmlFor="name">Seu nome</label>
+          <Input
+            type="text"
+            icon={FaRegUserCircle}
+            id="name"
+            placeholder="Seu nome da Silva"
+          />
+        </div>
+        <div className="input-wrapper">
+          <label htmlFor="email">Email</label>
+          <Input
+            type="text"
+            icon={FaEnvelope}
+            id="email"
+            placeholder="exemplo@exemplo.com.br"
+          />
+        </div>
+
+        <div className="input-wrapper">
+          <label htmlFor="password">Senha</label>
+          <Input
+            type="text"
+            icon={FaKey}
+            id="password"
+            placeholder="Senha de no mínimo 6 caracteres"
+          />
+        </div>
+
+        <Button title={'Criar conta'} />
+        <ButtonText title={'Já tenho uma conta'} />
+      </Form>
+    </Container>
+  )
+}
