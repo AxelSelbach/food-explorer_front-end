2727a32c49bcce8bd94fb5c19b4853ea2569b59b
axel <axelselbach90@gmail.com>
2023-12-06 05:22:18 -0300

    chore: added border none

--- src/components/ButtonText/styles.js
+++ src/components/ButtonText/styles.js
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 export const Container = styled.button`
   background: none;
   color: ${({ theme }) => theme.COLORS.COLOR_WHITE};
+  border: none;
 
   font-size: 24px;
 `
