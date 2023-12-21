11ee6a64b4842d0c03bee2f9f3852ebb60821992
axel <axelselbach90@gmail.com>
2023-12-21 18:44:09 -0300

    feat: created routes context

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ThemeProvider } from 'styled-components'
-import { DishInfo } from './pages/DishInfo'
+import { AuthProvider } from './hooks/auth'
+import { Routes } from './routes'
 import GlobalStyles from './styles/global'
 import theme from './styles/theme'
 
@@ -9,7 +10,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <DishInfo />
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
     </ThemeProvider>
   </React.StrictMode>,
 )
