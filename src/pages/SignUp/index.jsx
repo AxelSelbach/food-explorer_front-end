b994a80705d748a8d0ac2a833c333268d3655f6c
axel <axelselbach90@gmail.com>
2023-12-21 19:09:45 -0300

    feat: done sign up page

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,11 +1,40 @@
+/* eslint-disable prettier/prettier */
+import { useState } from 'react'
 import { FaEnvelope, FaKey, FaRegUserCircle } from 'react-icons/fa'
 import { FiCoffee } from 'react-icons/fi'
+import { Link, useNavigate } from 'react-router-dom'
 import { Button } from '../../components/Button'
 import { ButtonText } from '../../components/ButtonText'
 import { Input } from '../../components/Input'
+import { api } from '../../services/api'
 import { Container, Form } from './styles'
 
 export function SignUp() {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const navigate = useNavigate()
+
+  function handleSignUp() {
+    if (!name || !email || !password) {
+      return alert('Preencha todos os campos!')
+    }
+
+
+    api
+      .post('/users', { name, email, password })
+      .then(() => {
+        alert('Usuário cadastrado com sucesso!')
+        navigate('-1')
+      })
+      .catch((error) => {
+        if (error.response) {
+          alert(error.response.data.message)
+        } else {
+          alert('Nâo foi possível cadastrar')
+        }
+      })
+  }
   return (
     <Container>
       <Form>
@@ -21,6 +50,7 @@ export function SignUp() {
             icon={FaRegUserCircle}
             id="name"
             placeholder="Seu nome da Silva"
+            onChange={e => setName(e.target.value)}
           />
         </div>
         <div className="inputWrapper">
@@ -30,21 +60,25 @@ export function SignUp() {
             icon={FaEnvelope}
             id="email"
             placeholder="exemplo@exemplo.com.br"
+            onChange={e => setEmail(e.target.value)}
           />
         </div>
 
         <div className="inputWrapper">
           <label htmlFor="password">Senha</label>
           <Input
-            type="text"
+            type="password"
             icon={FaKey}
             id="password"
             placeholder="Senha de no mínimo 6 caracteres"
+            onChange={e => setPassword(e.target.value)}
           />
         </div>
 
-        <Button title={'Criar conta'} />
-        <ButtonText title={'Já tenho uma conta'} />
+        <Button title={'Criar conta'} onClick={handleSignUp} />
+        <Link to="/">
+          <ButtonText title={'Já tenho uma conta'} />
+        </Link>
       </Form>
     </Container>
   )
