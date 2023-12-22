9d68b8aeab4a0a5a916a22000cced029cf3ad1ed
axel <axelselbach90@gmail.com>
2023-12-22 10:21:50 -0300

    fix: swiper slide added in carousel, removed from card

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import { FaChevronRight, FaMinus, FaPlus, FaRegHeart } from 'react-icons/fa'
-import { SwiperSlide } from 'swiper/react'
 import { Button } from '../Button'
 import { Container, Content } from './styles'
 
@@ -8,32 +7,30 @@ export function Card({ picture, name, price, quantity, favorite }) {
   const [favorited, setFavorited] = useState(favorite)
 
   return (
-    <SwiperSlide>
-      <Container>
-        <Content>
-          <button className="favoriteButton">
-            <FaRegHeart />
-          </button>
-          <div className="cardBody">
-            <img src={picture} alt={`Prato de ${name}`} />
-            <h2>
-              {name}
-              <FaChevronRight />
-            </h2>
-            <p>R$ {price}</p>
-            <div className="quantitySelect">
-              <button className="minusBtn">
-                <FaMinus />
-              </button>
-              <span>{quantity}</span>
-              <button className="plusBtn">
-                <FaPlus />
-              </button>
-            </div>
+    <Container>
+      <Content>
+        <button className="favoriteButton">
+          <FaRegHeart />
+        </button>
+        <div className="cardBody">
+          <img src={picture} alt={`Prato de ${name}`} />
+          <h2>
+            {name}
+            <FaChevronRight />
+          </h2>
+          <p>R$ {price}</p>
+          <div className="quantitySelect">
+            <button className="minusBtn">
+              <FaMinus />
+            </button>
+            <span>{quantity}</span>
+            <button className="plusBtn">
+              <FaPlus />
+            </button>
           </div>
-          <Button title={'incluir'} />
-        </Content>
-      </Container>
-    </SwiperSlide>
+        </div>
+        <Button title={'incluir'} />
+      </Content>
+    </Container>
   )
 }
