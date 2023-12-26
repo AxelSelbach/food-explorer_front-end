f6ef0297badc3e22ef8b715c69c17bba9bc035c0
axel <axelselbach90@gmail.com>
2023-12-26 20:02:16 -0300

    refactor: added buttons function

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,10 +1,19 @@
 import { useState } from 'react'
 import { FaChevronRight, FaMinus, FaPlus, FaRegHeart } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
 import { Button } from '../Button'
 import { Container, Content } from './styles'
 
-export function Card({ picture, name, price, quantity, favorite }) {
-  const [favorited, setFavorited] = useState(favorite)
+export function Card({ picture, name, price }) {
+  const [quantity, setQuantity] = useState(1)
+
+  function increment() {
+    if (quantity >= 0) setQuantity(quantity + 1)
+  }
+
+  function decrement() {
+    if (quantity > 0) setQuantity(quantity - 1)
+  }
 
   return (
     <Container>
@@ -20,16 +29,18 @@ export function Card({ picture, name, price, quantity, favorite }) {
           </h2>
           <p>R$ {price}</p>
           <div className="quantitySelect">
-            <button className="minusBtn">
+            <button className="minusBtn" onClick={decrement}>
               <FaMinus />
             </button>
             <span>{quantity}</span>
-            <button className="plusBtn">
+            <button className="plusBtn" onClick={increment}>
               <FaPlus />
             </button>
           </div>
         </div>
-        <Button title={'incluir'} />
+        <Link to="/">
+          <Button title={'incluir'} />
+        </Link>
       </Content>
     </Container>
   )
