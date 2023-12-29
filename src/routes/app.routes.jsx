7fba61e31ce1d45c2530e55129245baa523b3de2
axel <axelselbach90@gmail.com>
2023-12-29 18:39:59 -0300

    feat: added page to create new dish to routes

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 /* import { useAuth } from '../hooks/auth' */
 import { Home } from '../pages/Home'
 import { DishInfo } from '../pages/DishInfo'
+import { AddDish } from '../pages/AddDish'
 
 export function AppRoutes() {
   /*   const { user } = useAuth()
@@ -11,6 +12,7 @@ export function AppRoutes() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/details/:id" element={<DishInfo />} />
+      <Route path="/add" element={<AddDish />} />
     </Routes>
   )
 }
