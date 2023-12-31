6b496f198a6bec26dcc66ccc26ec2bd530461a10
axel <axelselbach90@gmail.com>
2023-12-31 11:20:10 -0300

    chore: removed esLint from the page

--- src/components/IngredientTag/index.jsx
+++ src/components/IngredientTag/index.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { FaPlus, FaTimes } from 'react-icons/fa'
 
 import { Container } from './styles'
@@ -5,9 +6,18 @@ import { Container } from './styles'
 export function IngredientTag({ isNew, value, onClick, ...rest }) {
   return (
     <Container isnew={isNew ? 1 : 0}>
-      <input type="text" value={value} readOnly={!isNew} {...rest} />
+      <input
+        type="text"
+        value={value}
+        readOnly={!isNew}
+        {...rest}
+        required 
+      />
 
-      <button type="button" onClick={onClick}>
+      <button
+        type="button"
+        onClick={onClick}
+      >
         {isNew ? <FaPlus /> : <FaTimes />}
       </button>
     </Container>
