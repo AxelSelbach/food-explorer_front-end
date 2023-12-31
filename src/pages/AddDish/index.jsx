e2588c4322dff2af4b39180232853ad9f0fc65cb
axel <axelselbach90@gmail.com>
2023-12-31 18:03:45 -0300

    feat: created add dish page

--- src/pages/AddDish/index.jsx
+++ src/pages/AddDish/index.jsx
@@ -1,40 +1,176 @@
+import { useState } from 'react'
 import { FaAngleLeft, FaUpload } from 'react-icons/fa'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import { Button } from '../../components/Button'
 import { ButtonText } from '../../components/ButtonText'
 import { Footer } from '../../components/Footer'
 import { Header } from '../../components/Header'
-import { Input } from '../../components/Input'
-import { Container, Content } from './styles'
+import { IngredientTag } from '../../components/IngredientTag'
+import { Select } from '../../components/Select'
+import { api, imagesAPI } from '../../services/api'
+import {
+  BackLink,
+  Container,
+  Content,
+  IngredientsWrapper,
+  InputFile,
+} from './styles'
 
 export function AddDish() {
+  const [picture, setPicture] = useState('')
+  const [name, setName] = useState('')
+  const [category, setCategory] = useState('')
+  const [price, setPrice] = useState('')
+  const [description, setDescription] = useState('')
+  const [ingredients, setIngredients] = useState([])
+  const [newIngredient, setNewIngredient] = useState('')
+
+  const navigate = useNavigate()
+
+  function handleRemoveIngredient(deleted) {
+    setIngredients((prevState) =>
+      prevState.filter((ingredient) => ingredient !== deleted),
+    )
+  }
+
+  function handleAddLink() {
+    if (ingredients.includes(newIngredient)) {
+      toast.error('Ingrediente duplicado!')
+      return
+    }
+
+    if (ingredients.length === 0 || newIngredient === '') {
+      toast.error('Ingrediente Vazio / Inexistente')
+      return
+    }
+
+    setIngredients((prevState) => [...prevState, newIngredient])
+    setNewIngredient('')
+  }
+
+  async function handleAddDish() {
+    if (
+      !picture ||
+      !name ||
+      !category ||
+      !ingredients ||
+      !price ||
+      !description
+    ) {
+      toast.info('Para fazer o envio, é necessário preencher todos os campos!')
+      return
+    }
+
+    const formData = new FormData()
+    formData.append('photo', picture)
+    try {
+      toast.promise(
+        imagesAPI.post('', formData).then(
+          (res) =>
+            api.post('/dishes', {
+              picture: res.data.url,
+              name,
+              category,
+              ingredients,
+              price,
+              description,
+            }),
+          toast.success('Prato criado com sucesso!'),
+          navigate('-1'),
+        ),
+      )
+    } catch (error) {
+      if (error.response) {
+        return toast.error(
+          'Erro ao adicionar novo prato, tente novamente mais tarde',
+        )
+      }
+    }
+  }
+
   return (
     <Container>
       <Header />
       <Content>
-        <ButtonText icon={FaAngleLeft} title={'Voltar'} />
+        <BackLink to="/">
+          <ButtonText
+            // eslint-disable-next-line prettier/prettier
+            icon={FaAngleLeft}
+            title={'Voltar'}
+          />
+        </BackLink>
+
         <form>
           <h1>Novo Prato</h1>
           <fieldset>
-            <label htmlFor="picture">
-              Imagem do prato
-              <Input
-                icon={FaUpload}
-                title={'Selecione imagem'}
-                type="file"
-                id="picture"
-                required
-              />
-            </label>
+            <label htmlFor="picture">Imagem do prato</label>
+            <InputFile>
+              <FaUpload />
+              <label htmlFor="picture">Selecione uma imagem</label>
+              <input className="inputFile" type="file" id="picture" />
+            </InputFile>
 
-            <label htmlFor="name">
-              Nome
-              <Input
-                placeholder={'Ex.: Salada Ceasar'}
-                type="text"
-                id="name"
-                required
-              />
-            </label>
+            <label htmlFor="name">Nome</label>
+            <input
+              placeholder={'Ex.: Salada Ceasar'}
+              type="text"
+              id="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
+
+            <Select
+              title={'Categoria'}
+              onChange={(e) => setCategory(e.target.value)}
+            />
           </fieldset>
+          <fieldset>
+            <label htmlFor="ingredients">Ingredients</label>
+            <IngredientsWrapper>
+              {ingredients.map((ingredient, index) => (
+                // eslint-disable-next-line prettier/prettier
+                <IngredientTag
+                  key={String(index)}
+                  value={ingredient}
+                  onClick={() => handleRemoveIngredient(ingredient)}
+                />
+              ))}
+              <IngredientTag
+                placeholder="Adicionar"
+                isNew
+                id="ingredients"
+                value={newIngredient}
+                // eslint-disable-next-line prettier/prettier
+                onChange={e => setNewIngredient(e.target.value)}
+                onClick={handleAddLink}
+              />
+            </IngredientsWrapper>
+            <label htmlFor="price">Preço</label>
+            <input
+              type="number"
+              id="price"
+              placeholder="R$ 00,00"
+              onChange={(e) => setPrice(e.target.value)}
+              required
+            />
+          </fieldset>
+          <label htmlFor="description">Descrição</label>
+          <textarea
+            id="description"
+            placeholder="Fale brevemente sobre o prato, seus ingredientes e composição"
+            cols="30"
+            rows="10"
+            onChange={(e) => setDescription(e.target.value)}
+            required
+          ></textarea>
+          <Button
+            title="Criar Prato"
+            type={'submit'}
+            backgroundcolor={'#AB4D55'}
+            onClick={handleAddDish}
+          />
         </form>
       </Content>
       <Footer />
--- src/pages/AddDish/styles.js
+++ src/pages/AddDish/styles.js
@@ -1,24 +1,101 @@
+import { Link } from 'react-router-dom'
 import styled from 'styled-components'
+import { fadeIn } from '../../components/Header/styles'
 
 export const Container = styled.div`
   width: 100dvw;
   height: 100dvh;
-  padding: 24px 32px;
-  display: grid;
-  grid-template-rows: 10dvh 80dvh 10dvh;
-  grid-template-areas:
-    'header'
-    'content'
-    'footer';
 
+  overflow: auto;
+  color: white;
   grid-area: content;
 `
 
 export const Content = styled.div`
-  width: 100%;
-  height: 100%;
-
   display: flex;
   flex-direction: column;
   gap: 24px;
+
+  width: 100%;
+  padding: 24px 32px;
+
+  opacity: 0;
+  animation: ${fadeIn} 300ms linear forwards;
+
+  h1 {
+    font-family: Roboto;
+    font-size: 32px;
+  }
+
+  input,
+  textarea {
+    padding: 12px 14px;
+
+    font-family: Roboto;
+    font-size: 16px;
+    font-style: normal;
+
+    background-color: ${({ theme }) => theme.COLORS.BACKGROUND_600};
+    border: none;
+    border-radius: 8px;
+
+    color: ${({ theme }) => theme.COLORS.COLOR_WHITE};
+
+    resize: none;
+
+    &:focus {
+      outline: none;
+    }
+  }
+
+  label {
+    color: ${({ theme }) => theme.COLORS.PEARL_GRAY};
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+    gap: 24px;
+
+    fieldset {
+      display: flex;
+      flex-direction: column;
+      gap: 16px;
+      border: none;
+    }
+  }
+`
+
+export const InputFile = styled.div`
+  width: 100%;
+  background-color: ${({ theme }) => theme.COLORS.BACKGROUND_600};
+  padding: 12px 32px;
+  border-radius: 8px;
+
+  display: flex;
+  align-items: baseline;
+  gap: 8px;
+
+  input {
+    display: none;
+  }
+`
+export const IngredientsWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 16px;
+  width: 100%;
+  padding: 8px 8px;
+
+  background-color: ${({ theme }) => theme.COLORS.BACKGROUND_600};
+  border: none;
+  border-radius: 8px;
+`
+export const BackLink = styled(Link)`
+  width: fit-content;
+  color: ${({ theme }) => theme.COLORS.LIGHT_GRAY};
+
+  > button {
+    text-decoration: none;
+  }
 `
