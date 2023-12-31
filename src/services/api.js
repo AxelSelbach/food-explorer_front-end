fa6512f2e7d6f6a727a0bc66a2e45c5a609aa9b4
axel <axelselbach90@gmail.com>
2023-12-31 18:04:06 -0300

    feat: added images db

--- src/services/api.js
+++ src/services/api.js
@@ -3,3 +3,8 @@ import axios from 'axios'
 export const api = axios.create({
   baseURL: 'http://localhost:3000',
 })
+
+export const imagesAPI = axios.create({
+  baseURL:
+    'https://black_hole-3kf-1-y7475786.deta.app/api/integration/rk8nimlzmyls',
+})
