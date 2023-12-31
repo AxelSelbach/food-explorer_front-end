1d30874aa05a7c477e2fec6403d05e54c9138aec
axel <axelselbach90@gmail.com>
2023-12-31 18:03:17 -0300

    style: changed ingredient width

--- src/components/IngredientTag/styles.js
+++ src/components/IngredientTag/styles.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 export const Container = styled.div`
   display: flex;
   align-items: center;
-  width: 50%;
+  width: 45%;
 
   border-radius: 8px;
   padding: 4px 8px;
