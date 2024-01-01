1115a6730c77e62466558a846465072820d960d3
axel <axelselbach90@gmail.com>
2024-01-01 02:20:55 -0300

    feat: added add dish route for admin route

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -1,18 +1,18 @@
 import { Routes, Route } from 'react-router-dom'
-/* import { useAuth } from '../hooks/auth' */
+import { useAuth } from '../hooks/auth'
 import { Home } from '../pages/Home'
 import { DishInfo } from '../pages/DishInfo'
 import { AddDish } from '../pages/AddDish'
 
 export function AppRoutes() {
-  /*   const { user } = useAuth()
-  const isAdmin = user && user.isAdmin === 1 */
+  const { user } = useAuth()
+  const isAdmin = user && user.isAdmin === 1
 
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/details/:id" element={<DishInfo />} />
-      <Route path="/add" element={<AddDish />} />
+      {isAdmin && <Route path="/add" element={<AddDish />} />}
     </Routes>
   )
 }
