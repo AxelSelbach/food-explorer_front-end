ee67d94bde0c8972cac7a25c3bfca7a6e2222ef3
axel <axelselbach90@gmail.com>
2024-01-01 19:49:52 -0300

    feat: configured toastify on app

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import { ThemeProvider } from 'styled-components'
 import { AuthProvider } from './hooks/auth'
 import { Routes } from './routes'
@@ -13,6 +15,18 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <AuthProvider>
         <Routes />
       </AuthProvider>
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
     </ThemeProvider>
   </React.StrictMode>,
 )
